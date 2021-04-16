1ca9f5dc043cae1cff62c85856ad03943e64805c
Ledas-dev <Legals92@mail.ru>
2021-04-17 00:16:32 +0300

    sidebar

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import styles from '../styles/Home.module.css'
 import HeaderComponent from '../components/HeaderComponent/'
+import SidePanel from '../components/Panel/'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <HeaderComponent/>
+      <SidePanel>
+        <HeaderComponent/>
+      </SidePanel>
+
     </div>
   )
 }
