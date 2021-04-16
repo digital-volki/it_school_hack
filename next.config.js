869d85d21990ca17688481d61da21f722f54cf2f
Vlad.sh <shkerdin.v@yandex.ru>
2021-04-16 19:24:33 +0300

    init

---
+++ next.config.js
@@ -0,0 +1,36 @@
+require('webpack');
+const withOffline = require('next-offline')
+
+module.exports = (phase, { defaultConfig }) => {
+
+    // const pwa = withPWA({
+    //     pwa: {
+    //         dest: "public",
+    //         register: true,
+    //         skipWaiting: true
+    //     }
+    // })
+    return withOffline({...Object.assign({}, defaultConfig, {
+            webpack(config, options) {
+                config.module.rules.push({
+                    test: /\.(graphql|gql)$/,
+                    use: [require.resolve('graphql-tag/loader')]
+                });
+
+                if (typeof defaultConfig.webpack === "function") {
+                    return defaultConfig.webpack(config, options);
+                }
+                // config.pwa =  {
+                //     dest: "public",
+                //         register: true,
+                //         skipWaiting: true
+                // }
+
+                return config;
+            }
+        }), pwa:{
+            dest: "public",
+            register: true,
+            skipWaiting: true
+        }});
+}
