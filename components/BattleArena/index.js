12550a2530994f2f5560bda76c9e8bbb8dfc09ad
Ledas-dev <Legals92@mail.ru>
2021-04-17 09:25:44 +0300

    battle arena

---
+++ components/BattleArena/index.js
@@ -0,0 +1,11 @@
+import styles from './battle.module.scss'
+
+export default function BattleArena (){
+  return(
+    <>
+      <div className={styles.batteleArena}>
+        
+      </div>
+    </>
+  )
+}
