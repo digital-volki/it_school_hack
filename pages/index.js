51e3226a7536409cc65d73ca4ff7d7eeb478460a
Ledas-dev <Legals92@mail.ru>
2021-04-17 08:50:42 +0300

    panel

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import styles from '../styles/Home.module.css'
 import HeaderComponent from '../components/HeaderComponent/'
 import SideBar from '../components/SideBar/'
+import PlayerChallenge from '../components/PlayerChallenge/'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <SideBar>
         <HeaderComponent/>
+        <PlayerChallenge/>
       </SideBar>
 
     </div>
